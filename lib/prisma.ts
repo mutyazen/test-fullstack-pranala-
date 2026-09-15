080e59050834b8f33b0f7e2a6f9fdf86923087a4
meutyasuryadana <meutyasuryadana@gmail.com>
2026-09-15 12:08:16 +0700

    .

---
+++ lib/prisma.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+
+const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
+
+const prisma = new PrismaClient({
+  adapter,
+});
+
+export default prisma;
